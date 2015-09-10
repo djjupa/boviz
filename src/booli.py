ead6a13983c6b30d73f4a9a8f7a3fc130ec5461f
Julio Angulo <julio.angulo@kau.se>
2015-09-10 16:48:37 +0200

    data retrieved from booli. Python script working

--- src/booli.py
+++ src/booli.py
@@ -4,30 +4,36 @@ from hashlib import sha1
 import random
 import string
 import json
-from flask import Blueprint, make_response, jsonify, request
+#from flask import Blueprint, make_response, jsonify, request
 from sys import argv
 
 import os
 
 # Get the current directory
 current_dir = os.getcwd();
-print "Current directory: " + current_dir
+print "Current directory: " + current_dir;
+data_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "/data";
 
+print "Data directory: " + data_dir;
 
 ## Setting up initial variables
 
 #Get the path and name of the file where the output data will be stored 
-# filename = current_dir + "/data/booli.json" 
-filename = "/Users/djjupa/Projects/git/Booli/data/booli.json"
+filename = data_dir + "/booli.json";
+#filename = "/Users/djjupa/Projects/git/Booli/data/booli.json"
 
 
+total_objects = 1;
 offset_ini = 0;
-limit_ini = 100;
+limit_ini = 250;
 sleeping_time = 10;
 status_Sold = "sold";
 status_ForSale = "listings";
+search_Query = "Karlstad"
 
-def callBooli(status_Sold, offset, limit):
+
+
+def callBooli(status_Sold, search_Query, offset, limit):
 
     # Setup the stuff
     callerId = "DataAfterLife"
@@ -38,7 +44,7 @@ def callBooli(status_Sold, offset, limit):
     
     # The request string
     
-    url = "/"+status_Sold+"?q=karlstad&callerId="+callerId+"&time="+timestamp+"&unique="+unique+"&hash="+hashstr+"&offset="+str(offset)+"&limit="+str(limit)
+    url = "/"+status_Sold+"?q="+search_Query+"&callerId="+callerId+"&time="+timestamp+"&unique="+unique+"&hash="+hashstr+"&offset="+str(offset)+"&limit="+str(limit)
     # Setup and open the connection
     connection = httplib.HTTPConnection("api.booli.se")
     connection.request("GET", url)
@@ -47,11 +53,19 @@ def callBooli(status_Sold, offset, limit):
     connection.close()
     
     if response.status != 200:
-        print "fail"
+        print "fail";
     
-    result = data
+    result = data;
     
-    result_pretty_json = json.loads(result)
+    result_pretty_json = json.loads(result);
+    
+    # get the number of objects (houses) according to the json string
+    global total_objects;   # tell python that this variable is global 
+    total_objects = result_pretty_json['totalCount'];
+    print "Total number of housing objects: " + str(total_objects);
+    print "Offset: " + str(offset);
+    print "Limit: " + str(limit);
+    
     
     # Open the stream to write to a file
     target = open(filename, 'a')
@@ -64,12 +78,19 @@ def callBooli(status_Sold, offset, limit):
     
 # END of function
     
-# First call -- Do a loop here and increase the offsets
-callBooli(status_Sold,offset_ini, limit_ini);
+# Call the API until all of the objects (houses) has been retrieved
+while(offset_ini < total_objects):
+    
+    callBooli(status_Sold, search_Query, offset_ini, limit_ini);
 
-# Preparing for the next call 
-offset_ini = offset_ini + limit_ini;
-time.sleep(sleeping_time);
+    # Preparing for the next call 
+    offset_ini = offset_ini + limit_ini;
+    print "Total number of housing objects: " + str(total_objects);
+    print "Offset_ini: " + str(offset_ini);
+    print "Limit_ini: " + str(limit_ini);
+    
+    
+    time.sleep(sleeping_time);
     
     
 
